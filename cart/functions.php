dfd101e88f3033aab38e2eeebc44a171d5ecc978
Konrad <kondykox@gmail.com>
2023-08-11 14:44:26 +0200

    Code changes

--- cart/functions.php
+++ cart/functions.php
@@ -13,12 +13,30 @@ if (isset($_POST['action'])) {
     if ($action === 'changeAmount') {
         $newAmount = $_POST['newAmount'];
 
-        $sql = "UPDATE cart SET amount = $newAmount WHERE product_id = $productID AND user_id = $sesID;";
-        $result = mysqli_query($link, $sql);
+        $sql = "UPDATE cart SET amount = ? WHERE product_id = ? AND user_id = ?";
+        $stmt = mysqli_prepare($link, $sql);
+        mysqli_stmt_bind_param($stmt, "iii", $newAmount, $productID, $sesID);
+        $result = mysqli_stmt_execute($stmt);
+
+        if (!$result) {
+            // Obsługa błędu
+            echo "Wystąpił błąd przy aktualizacji ilości.";
+        }
+
+        mysqli_stmt_close($stmt);
     }
     // Delete item from cart
     elseif ($action === 'deleteItem') {
-        $sql = "DELETE FROM cart WHERE product_id = $productID AND user_id = $sesID;";
-        $result = mysqli_query($link, $sql);
+        $sql = "DELETE FROM cart WHERE product_id = ? AND user_id = ?";
+        $stmt = mysqli_prepare($link, $sql);
+        mysqli_stmt_bind_param($stmt, "ii", $productID, $sesID);
+        $result = mysqli_stmt_execute($stmt);
+
+        if (!$result) {
+            // Obsługa błędu
+            echo "Wystąpił błąd przy usuwaniu przedmiotu z koszyka.";
+        }
+
+        mysqli_stmt_close($stmt);
     }
 }
